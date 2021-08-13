a5436cae9c94ec16f34c10655444d65e43067b02
TheStealer <k_hintze_m@hotmail.com>
2021-08-12 20:25:19 -0500

    actualizar

--- DataBase/models/Tweet.js
+++ DataBase/models/Tweet.js
@@ -1,18 +1,17 @@
 cols = {
-    id: {
-        type: DataTypes.INTEGER
-    },
-    user: {
-        type: DataTypes.STRING
-    },
-    contenido: {
-        DataTypes.TEXT
-    },
-}
-const Tweet = sequelize.define("Tweet",
- cols, {
-     tableName: 'tweets',
-     timestamps: true,
- })
+  id: {
+    type: DataTypes.INTEGER,
+  },
+  user: {
+    type: DataTypes.STRING,
+  },
+  contenido: {
+    type: DataTypes.TEXT,
+  },
+};
+const Tweet = sequelize.define("Tweet", cols, {
+  tableName: "tweets",
+  timestamps: true,
+});
 
 return Tweet;
